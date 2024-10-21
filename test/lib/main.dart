fcc3354257cb5e44f441d978c9aa9e9bae6c21e2
NagawaEsther <nagawaesther227@gmail.com>
2024-10-21 14:53:04 +0300

    flutter_test

--- test/lib/main.dart
+++ test/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
 
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'), 
+      
       home: const HomeScreen(), 
       debugShowCheckedModeBanner: false,
       
